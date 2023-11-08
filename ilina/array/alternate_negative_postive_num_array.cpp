11f9f777082dd1e84f379637a61d0f074a9c4d84
Ilinapradhan <102991295+Ilinapradhan@users.noreply.github.com>
2023-11-08 21:18:42 +0530

    Create alternate_negative_postive_num_array.cpp
    given array of negative and positive no alternate the +ve and -ve no and make a new array .

---
+++ ilina/array/alternate_negative_postive_num_array.cpp
@@ -0,0 +1,33 @@
+void rearrange(int arr[], int n) {
+	  queue<int> ne;
+     queue<int> p;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0)
+            ne.push(arr[i]);
+        else if(arr[i]>=0){
+            p.push(arr[i]);
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        if(ne.size()==0){
+            arr[i]=p.front();
+            p.pop();
+            
+        }
+        else if(p.size()==0){
+            arr[i]=ne.front();
+            ne.pop();
+        }
+        else{
+            if(i%2==0){
+                arr[i]=p.front();
+                p.pop();
+            
+            }
+            else{
+                arr[i]=ne.front();
+                ne.pop();
+            }
+        }
+    }
+	}
