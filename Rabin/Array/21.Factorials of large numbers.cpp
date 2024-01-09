602dee54f1e35910c5f433ed492e3bfe6d9b30f8
Rabin <99532907+AgrawallaRabin@users.noreply.github.com>
2024-01-09 14:33:23 +0530

    Update 21.Factorials of large numbers.cpp

--- Rabin/Array/21.Factorials of large numbers.cpp
+++ Rabin/Array/21.Factorials of large numbers.cpp
@@ -1,35 +1,20 @@
- void multiply(vector<int>& nums, int &size, int &multiplier) {
-        int carry = 0;
-    
-        for(int i = 0; i<size; i++) {
-            int result = (multiplier*nums[i])+carry;
-    
-            nums[i]   = result%10;
-    
-            carry     = result/10;
-        }
-    
-        while(carry) {
-            nums[size] = carry%10;
-            size++;
-            carry /= 10;
-        }
-    }
-    
     vector<int> factorial(int N){
-        vector<int> nums(10000, 0);
-        nums[0] = 1;
-        int size = 1;
-        int fact = N;
-    
-    
-        for(int multiplier = 2; multiplier<=fact; multiplier++)
-            multiply(nums, size, multiplier);
-        
-        vector<int> result;
-        for(int i = size-1; i>=0 ; i--)
-            result.push_back(nums[i]);
-        return result;
+        vector<int> res;
+        res.push_back(1);
+        for(int i=2;i<=N;i++){
+            int carry = 0;
+            for(int j=0;j<res.size();j++){
+                int val = res[j]*i + carry;
+                res[j] = val%10;
+                carry = val/10;
+            }
+            while(carry!=0){
+                res.push_back(carry%10);
+                carry/=10;
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
 
 // Time Complexity : O(N2)
